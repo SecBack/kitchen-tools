79f1ab662f07c188d2264805dbb2e696c43d0e80
Albert <albert3257@live.dk>
2022-09-22 23:30:04 +0200

    add swipe tip to grocery list

--- kitchen-tools/src/app/grocery/grocery.page.ts
+++ kitchen-tools/src/app/grocery/grocery.page.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { GroceryService } from '../shared/services/grocery.service';
 import { Grocery } from '../shared/models/Grocery';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { IonItemSliding } from '@ionic/angular';
 
 @Component({
   selector: 'app-grocery',
   templateUrl: 'grocery.page.html',
   styleUrls: ['grocery.page.scss']
 })
-export class GroceryPage implements OnInit{
+export class GroceryPage implements OnInit {
+
+  @ViewChild('items') items: HTMLIonItemSlidingElement
+  @ViewChildren('sliding') itemList: QueryList<IonItemSliding>;
 
   /**
    * local observable which the html will subscribe to
@@ -26,31 +30,55 @@ export class GroceryPage implements OnInit{
    * on the data from our api
    *
    */
-  ngOnInit() {
+  ngOnInit() : void {
     // via the leftover post service get all posts from the api
     this.groceryService.getGroceries()
     // assign the local observable to the service observable. and just like that data updates are taken care of
     this.groceries = this.groceryService.groceries
+
+    this.showSwipeTip()
   }
 
   /**
-   * Change given grocery status to have, follow metod for further explanation
+   * Change the needed property of a given grocery, both on visually on frontend but also in the db. then
+   * close slider, for a more intutive user expirence
    *
-   * @param   {Grocery}  grocery  
+   * @param   {Grocery}  grocery  the given grocery
+   * @param   {string}   change   added or removed
    *
+   * @return  {void}
    */
-  groceryAdded(grocery: Grocery) {
-    this.groceryService.changeGrocery(grocery, true)
-    
+  changeGrocery(grocery: Grocery, change: string) : void {
+    if (change == 'added')
+      this.groceryService.changeGrocery(grocery, true)
+    if (change == 'removed')
+      this.groceryService.changeGrocery(grocery, false)
+
+    this.items.closeOpened()
+  }
+
+  timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
   /**
-   * Chnage given grocery status go needed, follow metod for further explanation
+   * This method shows the user a tip about how to use the grocery list. first waits a bit then it
+   * swipes first grocery element right then left to reveal the options. Teaching the user how to
+   * use the grocery list
    *
-   * @param   {Grocery}  grocery
-   *
+   * @return  {void}
    */
-  groceryRemoved(grocery: Grocery) {
-    this.groceryService.changeGrocery(grocery, false)
+  async showSwipeTip() {
+    await this.timeout(1500)
+    this.items.open('start')
+
+    await this.timeout(500)
+    this.items.closeOpened()
+
+    await this.timeout(100)
+    this.items.open('end')
+
+    await this.timeout(500)
+    this.items.closeOpened()
   }
 }
--- kitchen-tools/src/environments/environment.prod.ts
+++ kitchen-tools/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "http://10.0.2.2:3000/api"
+  apiUrl: "http://192.168.1.143:3000/api"// "http://10.0.2.2:3000/api" // physical is 10 virtual is 192
 };
